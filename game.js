df9961a54cf93577347900ec32643c7c9a0abf8e
Rodrigo <rodrigo070duarte@gmail.com>
2022-02-21 22:09:33 -0300

    pulish

--- game.js
+++ game.js
@@ -1,27 +1,27 @@
 function computerPlay() {
     choices = ['rock', 'paper', 'scissors'];
-    return choices[floor];
+    return choices[Math.floor(Math.random() * choices.length)];
 }
 
 function round(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     
-    switch (playerSelection)    {
-        case (playerSelection === computerSelection) :
-            return "It is a tie!";
-            break;
-        case 'rock' :
-            if (computerSelection === 'paper') {
-            return "You Lose! Paper beats Rock"
-        } else return "You Win! Rock beats Scissors";
-        break;
-        case 'paper' :
-            if (computerSelection === 'scissors') {
-                return "You Lose! Scissors beats Paper"
-            } else return "You Win! Paper beats Rock";
-        case 'scissors' :
-            if (computerSelection === 'rock') {
-                return "You Lose! Rock beats Scissors"
-            } else return "You Win! Scissors beats Paper";
-    }
-}
+    if (playerSelection === computerSelection) {
+        return"It is a tie!";
+    } else switch (playerSelection)    {
+            case 'rock' :
+                if (computerSelection === 'paper') {
+                    return"You Lose! Paper beats Rock"
+                } else return("You Win! Rock beats Scissors");
+            case 'paper' :
+                if (computerSelection === 'scissors') {
+                    return"You Lose! Scissors beats Paper"
+                } else return"You Win! Paper beats Rock";
+            case 'scissors' :
+                if (computerSelection === 'rock') {
+                    return "You Lose! Rock beats Scissors"
+                } else return"You Win! Scissors beats Paper";
+        }
+    }   
+
+console.log(round('Rock', computerPlay()))
