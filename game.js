6c808acdc7df5924fc97eb40417c28b044cd0a1e
Rodrigo <rodrigo070duarte@gmail.com>
2022-02-28 08:56:48 -0300

    js part done

--- game.js
+++ game.js
@@ -1,74 +1,75 @@
-// this function selects a random choice for the computer turn
+// this function selects a random choice for the computer
 function computerPlay() {
     choices = ['rock', 'paper', 'scissors'];
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
-// function to play the game
-function playRound(playerSelection, computerSelection) {
+function playRound(playerChoice, computerChoice) {
     turn += 1;
-    if (turn > 5) {
-        return endGame();
-    }
-    playerSelection = playerSelection.toLowerCase();
-    
-    if (playerSelection === computerSelection) {
-        return showResult('tie');
-    } else switch (playerSelection)    {
-        
+
+    if (playerChoice === computerChoice) {
+        showResult('tie');
+    } else switch (playerChoice)    {
         case 'rock' :
-            if (computerSelection === 'paper') {
-                return showResult('lose');
+            if (computerChoice === 'paper') {
+                showResult('lose');
             } else {
-            return showResult('win');
-        };
+                showResult('win');
+            };
+            break;
 
         case 'paper' :
-            if (computerSelection === 'scissors') {
-                return showResult('lose')
+            if (computerChoice === 'scissors') {
+                showResult('lose')
             } else {
-            return showResult('win');
-        };
+                showResult('win');
+            };
+            break;
             
         case 'scissors' :
-            if (computerSelection === 'rock') {
-                return showResult('lose');
+            if (computerChoice === 'rock') {
+                showResult('lose');
             } else {
-                return showResult('win');
+                showResult('win');
             };
+            break;
     }
-}   
+}
 
-function showResult(turnResult) {
-    if (turnResult == 'win') {
-        totalPlayerScore += totalPlayerScore + 1;
-    } else if (turnResult == 'lose') {
+function showResult(result) {
+    if (result === 'win') {
+        totalPlayerScore += + 1;
+    } else if (result ==='lose') {
         totalCompScore += 1;
     }
 
     const displayResult = document.createElement('div');
-    displayResult.textContent = turnResult;
-    result.appendChild(displayResult);
+    displayResult.textContent = result;
+    DIVresult.appendChild(displayResult);
+
+    if (turn === 5) {
+        endGame();
+    }
 }
 
 function endGame() {
     const displayEnd = document.createElement('div');
     displayEnd.textContent = `Final Score: ${totalPlayerScore} - ${totalCompScore}`;
-    result.appendChild(displayEnd);
     const buttonDisable = document.querySelectorAll('button');
     buttonDisable.forEach(btn => btn.disabled = true);
+
+    DIVresult.appendChild(displayEnd);
 }
 
+let turn = 0;
+let totalPlayerScore = 0;
+let totalCompScore = 0;
 
 btn_rock = document.querySelector('#btn-rock');
 btn_paper = document.querySelector('#btn-paper');
 btn_scissors = document.querySelector('#btn-scissors');
-result = document.querySelector('#result');
+DIVresult = document.querySelector('#result');
 
-let totalPlayerScore = 0;
-let totalCompScore = 0;
-let turn = 0;
-
-btn_rock.addEventListener('click', () => playRound('rock', computerPlay()));
-btn_paper.addEventListener('click', () => playRound('paper', computerPlay()));
+btn_rock.addEventListener('click', () => playRound('rock', 'paper'));
+btn_paper.addEventListener('click', () => playRound('paper', 'scissors'));
 btn_scissors.addEventListener('click', () => playRound('scissors', computerPlay()));
